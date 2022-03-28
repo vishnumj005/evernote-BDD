892f91df256776c589712685f97c5f7fc97a4897
Vishnu <vishnu@SSSLAP0097-Vishnumj.local>
2022-03-28 16:56:24 +0530

    Deletimg folder

--- Sample/src/Sample.java
+++
@@ -1,9 +0,0 @@
-import com.github.javafaker.Faker;
-
-public class Sample {
-	public static void main(String [ ] args) {
-		Faker faker = new Faker();
-		System.out.print(faker.lorem().sentence());
-
-	}
-}
